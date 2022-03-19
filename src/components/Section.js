8b1db6c37fb1347287ee1b870a8cb4a685337f5b
Dmitry Velichko <soleil-2005@mail.ru>
2022-03-19 23:21:49 +0300

    New commit

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,21 +4,19 @@ import styled from 'styled-components'
 function Section() {
   return (
     <Wrap>
-        <ItemText>
-            <h1>Model S</h1>
-            <p>Order Online for Touchless Delivery</p>
-        </ItemText>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <Buttons>
         <ButtonGroup>
-          <LeftButton>
-            Custom Order    
-          </LeftButton>
-          <RightButton>
-            Existing Inventory
-          </RightButton>
+          <LeftButton>Custom Order</LeftButton>
+          <RightButton>Existing Inventory</RightButton>
         </ButtonGroup>
         <DownArrow src='/images/down-arrow.svg' />
+      </Buttons>
     </Wrap>
-  )
+  );
 }
 
 export default Section
@@ -68,4 +66,8 @@ const RightButton = styled(LeftButton)`
 const DownArrow = styled.img`
   margin-top: 20px;
   height: 40px;
+`
+
+const Buttons = styled.div`
+
 `
