60f139682f63bc0d0af4ba8005aa1eb76dd21f4d
Dmitry Velichko <soleil-2005@mail.ru>
2022-03-20 23:11:13 +0300

    New commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,7 @@ function Home() {
         description='Order Online for Touchless Delivery'  
         backgroundImg='model-s.jpg'
         leftBtnText='Custom Order'
+        rightBtnText='Existing Inventory'
       />
       <Section />
       <Section />
